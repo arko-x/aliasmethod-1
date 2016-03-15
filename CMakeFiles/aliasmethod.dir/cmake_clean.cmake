7f9ad8c9a9c5b1bf22355ab5cbfd65d869857f7d
m-ochi <masanao.oochi@gmail.com>
2016-03-15 11:47:56 +0900

    first commit

---
+++ CMakeFiles/aliasmethod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aliasmethod.dir/aliastable.cpp.o"
+  "aliasmethod.pdb"
+  "aliasmethod"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aliasmethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
